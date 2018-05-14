289056599a83f25ba144e3bc4dd3d49790867319
Tom Van Mele <vanmelet@ethz.ch>
2018-05-14 11:16:27 +0200

    removed loadpat2 numpy for now

--- src/compas_ags/ags/__init__.py
+++ src/compas_ags/ags/__init__.py
@@ -1,5 +1,3 @@
-""""""
-
 from compas_ags.ags import graphstatics
 from compas_ags.ags import loadpath
 from compas_ags.ags import loadpath3_numpy
@@ -7,6 +5,7 @@ from compas_ags.ags import loadpath3_numpy
 from .graphstatics import *
 from .loadpath import *
 from .loadpath3_numpy import *
-from .loadpath2_numpy import *
+# from .loadpath2_numpy import *
 
-__all__ = graphstatics.__all__ + loadpath.__all__ + loadpath3_numpy.__all__ + loadpath2_numpy.__all__
+# __all__ = graphstatics.__all__ + loadpath.__all__ + loadpath3_numpy.__all__ + loadpath2_numpy.__all__
+__all__ = graphstatics.__all__ + loadpath.__all__ + loadpath3_numpy.__all__
--- src/compas_ags/diagrams/__init__.py
+++ src/compas_ags/diagrams/__init__.py
@@ -3,7 +3,7 @@
 from compas_ags.diagrams import formdiagram
 from compas_ags.diagrams import forcediagram
 
-from formdiagram import *
-from forcediagram import *
+from .formdiagram import *
+from .forcediagram import *
 
 __all__ = formdiagram.__all__ + forcediagram.__all__
