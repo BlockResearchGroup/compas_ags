ad3f036eb5ef30b6f7dfe4702279d7b91e1ce829
freeeki <freeeki@gmail.com>
2018-07-14 12:12:07 +0200

    cleanup

--- examples/example_lpopt.py
+++ examples/example_lpopt.py
@@ -2,9 +2,7 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-import json
-import os
-
+import compas
 import compas_ags
 
 from compas_ags.diagrams import FormDiagram
--- src/compas_ags/ags/__init__.py
+++ src/compas_ags/ags/__init__.py
@@ -1,10 +1,7 @@
 from compas_ags.ags import graphstatics
 from compas_ags.ags import loadpath
-from compas_ags.ags import loadpath3_numpy
 
 from .graphstatics import *
 from .loadpath import *
-from .loadpath3_numpy import *
-# from .loadpath2_numpy import *
 
-__all__ = graphstatics.__all__ + loadpath.__all__ + loadpath3_numpy.__all__
+__all__ = graphstatics.__all__ + loadpath.__all__
