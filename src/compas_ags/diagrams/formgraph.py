c545b1de911a861bf3cac7ba0b5b39d35485102a
Tom Van Mele <vanmelet@ethz.ch>
2020-07-20 21:08:21 +0200

    extended form graph methods re-added embedding part of fink example

--- src/compas_ags/diagrams/formgraph.py
+++ src/compas_ags/diagrams/formgraph.py
@@ -2,7 +2,9 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
+import compas
 from compas.datastructures import Network
+from compas.datastructures import network_is_crossed
 
 
 __all__ = ['FormGraph']
@@ -12,14 +14,41 @@ class FormGraph(Network):
 
     def __init__(self):
         super(FormGraph, self).__init__()
-        self.is_2d()
 
     def is_2d(self):
-        for key in self.nodes():
-            if self.node_attribute(key, 'z') != 0.0 :
-                print('Node %s not in xy plane. It will be projected to xy plane' % key)
-                self.node_attribute(key, 'z', 0.0)
+        z = self.nodes_attribute('z')
+        zmin = min(z)
+        zmax = max(z)
+        if (zmax - zmin) ** 2 > 0.001:
+            return False
         return True
+
+    def make_2d(self):
+        self.nodes_attribute('z', 0.0)
+
+    def is_planar(self):
+        if compas.IPY:
+            from compas.rpc import Proxy
+            planarity = Proxy('planarity')
+        else:
+            import planarity
+        return planarity.is_planar(list(self.edges()))
+
+    def is_crossed(self):
+        return network_is_crossed(self)
+
+    def is_planar_embedding(self):
+        return self.is_2d() and self.is_planar() and not self.is_crossed()
+
+    def embed(self, fixed=None, straightline=True):
+        if compas.IPY:
+            from compas.rpc import Proxy
+            proxy = Proxy('compas.datastructures')
+            def network_embed_in_plane(network, fixed, straightline):
+                network.data = proxy.network_embed_in_plane_proxy(network.data, fixed, straightline)
+        else:
+            from compas.datastructures import network_embed_in_plane
+        return network_embed_in_plane(self, fixed, straightline)
 
 
 # ==============================================================================
--- src/compas_ags/utilities/__init__.py
+++
@@ -1,4 +0,0 @@
-from .errorhandler import *
-from .helpers import *
-
-__all__ = errorhandler.__all__ + helpers.__all__
