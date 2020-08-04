240364adb3b40a447b4cd93c0e290057a935cfc2
Ricardo Maia Avelino <36137188+ricardoavelino@users.noreply.github.com>
2020-08-04 15:44:22 -0300

    clean examples

--- scripts/test_dof.py
+++ scripts/test_dof.py
@@ -5,15 +5,9 @@ from compas_ags.diagrams import FormDiagram
 # from compas_ags.ags import graphstatics
 from compas.rpc import Proxy
 
-proxy = Proxy()
-proxy.start_server()
-proxy.stop_server()
-
 graphstatics = Proxy('compas_ags.ags.graphstatics')
 
-path = '/Users/mricardo/compas_dev/compas_ags/data/paper/gs_form_force.obj'
-
-graph = FormGraph.from_obj(path)
+graph = FormGraph.from_obj(compas_ags.get('paper/gs_form_force.obj'))
 form = FormDiagram.from_graph(graph)
 
 dof = graphstatics.form_count_dof_proxy(form.data)
