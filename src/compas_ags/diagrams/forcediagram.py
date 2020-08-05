4f8bde9e6b67741a85ce88cb526162cb6a1d40ff
Tom Van Mele <vanmelet@ethz.ch>
2020-08-05 16:28:38 +0200

    first version of the where dual function

--- src/compas_ags/diagrams/forcediagram.py
+++ src/compas_ags/diagrams/forcediagram.py
@@ -72,21 +72,134 @@ class ForceDiagram(Diagram):
     # Helpers
     # --------------------------------------------------------------------------
 
+    def edges_where_dual(self, conditions, data=False):
+        """Get edges for which a certain condition or set of conditions is true for the corresponding edges in the diagram's dual.
+
+        Parameters
+        ----------
+        conditions : dict
+            A set of conditions in the form of key-value pairs.
+            The keys should be attribute names. The values can be attribute
+            values or ranges of attribute values in the form of min/max pairs.
+        data : bool, optional
+            Yield the edges and their data attributes.
+            Default is ``False``.
+
+        Yields
+        ------
+        2-tuple
+            The next edge as a (u, v) tuple, if ``data=False``.
+        3-tuple
+            The next edge as a (u, v, data) tuple, if ``data=True``.
+        """
+        for edge in self.edges():
+            is_match = True
+
+            dual_edge = self.dual_edge(edge)
+            dual_edge_attr = self.dual.edge_attributes(dual_edge)
+
+            for cond_name, cond_value in conditions.items():
+                dual_method = getattr(self.dual, cond_name, None)
+
+                if dual_method and callable(dual_method):
+                    dual_value = dual_method(dual_edge)
+                elif cond_name in dual_edge_attr:
+                    dual_value = dual_edge_attr[cond_name]
+                else:
+                    is_match = False
+                    break
+
+                if isinstance(dual_value, list):
+                    if cond_value not in dual_value:
+                        is_match = False
+                        break
+                elif isinstance(cond_value, (tuple, list)):
+                    minval, maxval = cond_value
+                    if dual_value < minval or dual_value > maxval:
+                        is_match = False
+                        break
+                else:
+                    if cond_value != dual_value:
+                        is_match = False
+                        break
+
+            if is_match:
+                if data:
+                    yield edge, self.edge_attributes(edge)
+                else:
+                    yield edge
+
     def dual_edge(self, edge):
+        """Find the cooresponding edge in the diagram's dual.
+
+        Parameters
+        ----------
+        edge : tuple of int
+            The edge identifier.
+
+        Returns
+        -------
+        tuple(int, int) or None
+            The identifier of the dual edge if it exists.
+        """
         for u, v in self.dual.face_halfedges(edge[0]):
             if self.dual.halfedge[v][u] == edge[1]:
                 return u, v
 
     def is_dual_edge_external(self, edge):
+        """Verify if the corresponding edge in the diagram's dual is marked as "external".
+
+        Parameters
+        ----------
+        edge : tuple of int
+            The edge identifier.
+
+        Returns
+        -------
+        bool
+        """
         return self.dual.edge_attribute(self.dual_edge(edge), 'is_external')
 
     def is_dual_edge_reaction(self, edge):
+        """Verify if the corresponding edge in the diagram's dual is marked as "reaction".
+
+        Parameters
+        ----------
+        edge : tuple of int
+            The edge identifier.
+
+        Returns
+        -------
+        bool
+        """
         return self.dual.edge_attribute(self.dual_edge(edge), 'is_reaction')
 
     def is_dual_edge_load(self, edge):
+        """Verify if the corresponding edge in the diagram's dual is marked as "load".
+
+        Parameters
+        ----------
+        edge : tuple of int
+            The edge identifier.
+
+        Returns
+        -------
+        bool
+        """
         return self.dual.edge_attribute(self.dual_edge(edge), 'is_load')
 
     def is_dual_edge_ind(self, edge):
+        """Verify if the corresponding edge in the diagram's dual is marked as "independent".
+
+        Parameters
+        ----------
+        edge : tuple of int
+            The edge identifier.
+
+        Returns
+        -------
+        bool
+        """
         return self.dual.edge_attribute(self.dual_edge(edge), 'is_ind')
 
     def edge_index(self, form=None):
