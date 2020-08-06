918a1e6d3acaa536a18d17e3e9a43765abe6c055
Ricardo Maia Avelino <36137188+ricardoavelino@users.noreply.github.com>
2020-08-06 11:23:38 -0300

    True/False Option for fix_vertices

--- ui/Rhino/AGS/dev/AGS_form_fix_nodes_cmd.py
+++ ui/Rhino/AGS/dev/AGS_form_fix_nodes_cmd.py
@@ -24,8 +24,16 @@ def RunCommand(is_interactive):
         vertices = form.select_vertices()
         if not vertices:
             break
-        for vertex in vertices:
-            form.diagram.vertex_attribute(vertex, 'is_fixed', True)
+        current = str(form.diagram.vertex_attribute(vertices[0], 'is_fixed'))  # display T/F based on first selected
+        show = compas_rhino.rs.GetString("Fix Nodes", defaultString=current, strings=["True", "False"])
+        if show == "True":
+            for vertex in vertices:
+                form.diagram.vertex_attribute(vertex, 'is_fixed', True)
+        elif show == "False":
+            for vertex in vertices:
+                form.diagram.vertex_attribute(vertex, 'is_fixed', False)
+        else:
+            pass
         scene.clear()
         scene.update()
 
