dcbf2c851095f4ca7c9c26c029633bd9210cb69f
duchaoyu <duch@student.ethz.ch>
2020-08-06 16:57:31 +0200

    - format - fixed_unfixed_nodes

--- ui/Rhino/AGS/dev/AGS_form_displaysettings_cmd.py
+++ ui/Rhino/AGS/dev/AGS_form_displaysettings_cmd.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 from __future__ import division
 
 import scriptcontext as sc
-
 import compas_rhino
 
 
--- ui/Rhino/AGS/dev/AGS_form_fix_nodes_cmd.py
+++ ui/Rhino/AGS/dev/AGS_form_fix_nodes_cmd.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 from __future__ import division
 
 import scriptcontext as sc
-
 import compas_rhino
 
 
@@ -24,10 +23,22 @@ def RunCommand(is_interactive):
         vertices = form.select_vertices()
         if not vertices:
             break
+        fixed = []
+        unfixed = []
         for vertex in vertices:
-            form.diagram.vertex_attribute(vertex, 'is_fixed', True)
-        scene.clear()
+            if form.diagram.vertex_attribute(vertex, 'is_fixed') is True:
+                form.diagram.vertex_attribute(vertex, 'is_fixed', False)
+                unfixed.append(vertex)
+            else:
+                form.diagram.vertex_attribute(vertex, 'is_fixed', True)
+                fixed.append(vertex)
         scene.update()
+        if fixed and unfixed: 
+            print(fixed, 'nodes are fixed,', unfixed, 'nodes are unfixed') 
+        if fixed and not unfixed:
+            print(fixed, 'nodes are fixed')
+        if unfixed and not fixed:
+            print(unfixed, 'nodes are unfixed')
 
 
 # ==============================================================================
