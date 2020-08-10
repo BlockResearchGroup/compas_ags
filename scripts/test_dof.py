583a4491706afaa03641a19721d310679d810088
duchaoyu <duch@student.ethz.ch>
2020-08-10 15:26:07 +0200

    - reverse change in test_dof

--- scripts/test_dof.py
+++ scripts/test_dof.py
@@ -2,6 +2,7 @@ import compas_ags
 
 from compas_ags.diagrams import FormGraph
 from compas_ags.diagrams import FormDiagram
+from compas_ags.ags import graphstatics
 from compas.rpc import Proxy
 
 graphstatics = Proxy('compas_ags.ags.graphstatics')
@@ -10,4 +11,5 @@ graph = FormGraph.from_obj(compas_ags.get('paper/gs_form_force.obj'))
 form = FormDiagram.from_graph(graph)
 
 dof = graphstatics.form_count_dof_proxy(form.data)
+
 print(dof)
