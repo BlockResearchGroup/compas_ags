e4cd0373f70b4e1a04602a8972ea1dfba3fa91a2
duchaoyu <duch@student.ethz.ch>
2020-08-10 15:26:51 +0200

    - test_loadpath

--- scripts/test_loadpath.py
+++ scripts/test_loadpath.py
@@ -5,14 +5,13 @@ from compas_ags.diagrams import ForceDiagram
 from compas_ags.diagrams import FormDiagram
 from compas.rpc import Proxy
 
-#graphstatics = Proxy('compas_ags.ags.graphstatics')
 
 graph = FormGraph.from_obj(compas_ags.get('paper/gs_form_force.obj'))
 form = FormDiagram.from_graph(graph)
 force = ForceDiagram.from_formdiagram(form)
 
 loadpath = Proxy('compas_ags.ags.loadpath')
-#dof = graphstatics.form_count_dof_proxy(form.data)
+
 lp = loadpath.compute_loadpath_proxy(form.data, force.data)
 print(lp)
-#print(dof)
+
