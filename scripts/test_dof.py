731574f0ce46a72c8743f23b7bcf045e5414fb49
duchaoyu <duch@student.ethz.ch>
2020-08-10 15:18:50 +0200

    - add loadpath_proxy function - new cmd: AGS_check_loadpath_cmd

--- scripts/test_dof.py
+++ scripts/test_dof.py
@@ -1,15 +1,18 @@
 import compas_ags
 
 from compas_ags.diagrams import FormGraph
+from compas_ags.diagrams import ForceDiagram
 from compas_ags.diagrams import FormDiagram
-# from compas_ags.ags import graphstatics
 from compas.rpc import Proxy
 
-graphstatics = Proxy('compas_ags.ags.graphstatics')
+#graphstatics = Proxy('compas_ags.ags.graphstatics')
 
 graph = FormGraph.from_obj(compas_ags.get('paper/gs_form_force.obj'))
 form = FormDiagram.from_graph(graph)
+force = ForceDiagram.from_formdiagram(form)
 
-dof = graphstatics.form_count_dof_proxy(form.data)
-
-print(dof)
+loadpath = Proxy('compas_ags.ags.loadpath')
+#dof = graphstatics.form_count_dof_proxy(form.data)
+lp = loadpath.compute_loadpath_proxy(form.data, force.data)
+print(lp)
+#print(dof)
---
+++ ui/Rhino/AGS/dev/AGS_check_loadpath_cmd.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import scriptcontext as sc
+
+import compas_rhino
+
+
+__commandname__ = "AGS_check_loadpath"
+
+
+def RunCommand(is_interactive):
+
+    if 'AGS' not in sc.sticky:
+        compas_rhino.display_message('AGS has not been initialised yet.')
+        return
+
+    proxy = sc.sticky['AGS']['proxy']
+    scene = sc.sticky['AGS']['scene']
+    form = scene.find_by_name('Form')[0]
+    force = scene.find_by_name('Force')[0]
+
+    proxy.package = 'compas_ags.ags.loadpath'
+    print(proxy.package)
+
+    lp = proxy.compute_loadpath_proxy(form.diagram.data, force.diagram.data)
+    print('Loadpath of the structure is {} kNm.'.format(lp))
+
+
+# ==============================================================================
+# Main
+# ==============================================================================
+
+if __name__ == '__main__':
+
+    RunCommand(True)
