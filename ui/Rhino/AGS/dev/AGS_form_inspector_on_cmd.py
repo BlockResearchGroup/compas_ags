1420112ff850f6cb9f949771219693c7c8668309
Tom Van Mele <vanmelet@ethz.ch>
2020-09-11 15:15:13 +0200

    removed inspector for form for now

--- ui/Rhino/AGS/dev/AGS_form_inspector_on_cmd.py
+++ ui/Rhino/AGS/dev/AGS_form_inspector_on_cmd.py
@@ -2,28 +2,29 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
-import scriptcontext as sc
+# import scriptcontext as sc
 
-import compas_rhino
+# import compas_rhino
 
 
 __commandname__ = "AGS_form_inspector_on"
 
 
 def RunCommand(is_interactive):
-    if 'AGS' not in sc.sticky:
-        compas_rhino.display_message('AGS has not been initialised yet.')
-        return
+    # if 'AGS' not in sc.sticky:
+    #     compas_rhino.display_message('AGS has not been initialised yet.')
+    #     return
 
-    scene = sc.sticky['AGS']['scene']
+    # scene = sc.sticky['AGS']['scene']
 
-    objects = scene.find_by_name('Form')
-    if not objects:
-        compas_rhino.display_message("There is no FormDiagram in the scene.")
-        return
-    form = objects[0]
+    # objects = scene.find_by_name('Form')
+    # if not objects:
+    #     compas_rhino.display_message("There is no FormDiagram in the scene.")
+    #     return
+    # form = objects[0]
 
-    form.inspector_on()
+    # form.inspector_on()
+    pass
 
 
 # ==============================================================================
