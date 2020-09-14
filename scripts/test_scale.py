2bd6a4a754e38fc140eaab428e40791122d34ae3
Ricardo Maia Avelino <36137188+ricardoavelino@users.noreply.github.com>
2020-09-14 20:07:00 +0200

    test script

--- scripts/test_scale.py
+++ scripts/test_scale.py
@@ -13,6 +13,8 @@ from compas_ags.diagrams import ForceDiagram
 from compas_ags.rhino import FormObject
 from compas_ags.rhino import ForceObject
 
+from compas_ags.utilities import calculate_drawingscale
+
 graphstatics = Proxy('compas_ags.ags.graphstatics')
 
 HERE = os.path.dirname(__file__)
@@ -38,11 +40,41 @@ force.data = graphstatics.force_update_from_form_proxy(force.data, form.data)
 form_object = FormObject(form, name="Form", layer="AGS::FormDiagram")
 force_object = ForceObject(force, name="Force", layer="AGS::ForceDiagram")
 
+scale = calculate_drawingscale(form, force)
+print(scale)
+
+print(force_object.location)
+
 form_object.clear_layer()
 force_object.clear_layer()
 
-force_object.location = [35, 0, 0]
-force_object.scale = 5.0
+# force_object.location = [35, 0, 0]
+force_object.scale = scale
+
+form_xyz = list(form_object.vertex_xyz.values())
+force_xyz = list(force_object.vertex_xyz.values())
+
+form_xmax = max([xyz[0] for xyz in form_xyz])
+form_xmin = min([xyz[0] for xyz in form_xyz])
+
+form_ymin = min([xyz[1] for xyz in form_xyz])
+
+force_xmin = min([xyz[0] for xyz in force_xyz])
+force_ymin = min([xyz[1] for xyz in force_xyz])
+
+#anchor_x, anchor_y = force_object.location[0], force_object.location[1]
+#danchor_x = anchor_x - force_xmin
+#danchor_y = anchor_y - force_ymin
+
+spacing = 0.5 * (form_xmax - form_xmin)
+
+force_object.location[0] += form_xmax + spacing - force_xmin
+force_object.location[1] += form_ymin - force_ymin
+
+#dx = form_xmax + spacing - force_xmin
+#force_object.location[0] += dx
+
+print(force_object.location)
 
 form_object.draw()
 force_object.draw()
@@ -55,5 +87,5 @@ force_object.draw()
 # if vertices and force_object.move_vertices(vertices):
 #     force_object.draw()
 
-if force_object.scale_from_3_points(message="Select the base node of the Force Diagram for the scaling operation"):
-    force_object.draw()
+#if force_object.scale_from_3_points(message="Select the base node of the Force Diagram for the scaling operation"):
+#    force_object.draw()
