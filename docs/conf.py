af31c3a6d2d1d383d0b2a721847ecec2a521e826
Li <leochen1990@gmail.com>
2020-11-03 15:43:38 +0100

    add version dropdown

--- docs/conf.py
+++ docs/conf.py
@@ -131,6 +131,12 @@ intersphinx_mapping = {
 
 # -- Options for HTML output ----------------------------------------------
 
+docs_site = "https://blockresearchgroup.github.io/compas_ags/"
+f = urllib.request. urlopen(docs_site + "doc_versions.txt")
+version_names = [version.decode("utf-8").strip() for version in f.readlines()]
+package_docs_versions = [(version, "{}{}".format(docs_site, version))
+                            for version in version_names if version]
+
 html_theme = 'compaspkg'
 html_theme_path = sphinx_compas_theme.get_html_theme_path()
 html_theme_options = {
@@ -140,7 +146,8 @@ html_theme_options = {
     "package_author": "Tom Van Mele",
     "package_description": "COMPAS package for Algebraic Graph Statics",
     "package_repo": "https://github.com/BlockResearchGroup/compas_ags",
-    "package_docs": "https://blockresearchgroup.github.io/compas_ags"
+    "package_docs": "https://blockresearchgroup.github.io/compas_ags",
+    "package_old_versions": package_docs_versions
 }
 html_context = {}
 html_static_path = []
