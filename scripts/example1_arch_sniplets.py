e6d065b4dd14ff14e1d3cbf74093fadc75fb6463
Ricardo Maia Avelino <36137188+ricardoavelino@users.noreply.github.com>
2021-03-18 21:00:07 +0100

    snippets - arch

--- scripts/example1_arch_sniplets.py
+++ scripts/example1_arch_sniplets.py
@@ -2,9 +2,6 @@ import compas_ags
 from compas_ags.diagrams import FormGraph
 from compas_ags.diagrams import FormDiagram
 from compas_ags.diagrams import ForceDiagram
-from compas_ags.ags import form_update_from_force
-from compas_ags.ags import ConstraintsCollection
-from compas_ags.ags import form_update_from_force
 
 
 # Sniplets syle
@@ -44,9 +41,9 @@ for index in edges_ind:
 #   3. create force diagram and compute equilibrium
 # ------------------------------------------------------------------------------
 
+from compas_ags.diagrams import ForceDiagram
 from compas_ags.ags import form_update_q_from_qind
 from compas_ags.ags import force_update_from_form
-
 
 # create a dual force diagram
 force = ForceDiagram.from_formdiagram(form)
@@ -62,7 +59,9 @@ viewer.draw_form(vertexlabel={key: key for key in form.vertices()},
                  vertexsize=0.2,
                  forces_on=True,
                  forcescale=0.5)
-viewer.draw_force(edgelabel={uv: index for index, uv in enumerate(force.edges())})
+viewer.draw_force(vertexlabel={key: key for key in force.vertices()},
+                  vertexsize=0.2,
+                  edgelabel={uv: index for index, uv in enumerate(force.edges())})
 viewer.show()
 
 # store lines representing the current state of equilibrium
@@ -87,12 +86,43 @@ for u, v in force.edges():
     })
 
 # --------------------------------------------------------------------------
-#   3. force diagram manipulation and modify the form diagram
+#   4. Set default constraints based on the direction of the external loads
 # --------------------------------------------------------------------------
+
+# Identify auto constraints
+form.identify_constraints()
+
+# Reflect all constraints to force diagram
+force.constraints_from_dual()
+
+force_edges = force.ordered_edges(form)
+
+force_edge_labels1 = {(u, v): index for index, (u, v) in enumerate(force_edges)}
+force_edge_labels2 = {(v, u): index for index, (u, v) in enumerate(force_edges)}
+force_edge_labels = {**force_edge_labels1, **force_edge_labels2}
+
+# viewer = Viewer(form, force, delay_setup=False)
+# viewer.draw_form(vertexlabel={key: key for key in form.vertices()},
+#                  edgelabel={uv: index for index, uv in enumerate(form.edges())},
+#                  vertexsize=0.2,
+#                  forces_on=False,
+#                  vertexcolor={key: '#FF0000' for key in form.vertices_where({'is_fixed_x': True})})
+# viewer.draw_force(edgelabel=force_edge_labels,
+#                  vertexcolor={key: '#FF0000' for key in force.vertices_where({'is_fixed_x': True})})
+# viewer.show()
+
+
+
+
+# --------------------------------------------------------------------------
+#   5. force diagram manipulation and modify the form diagram
+# --------------------------------------------------------------------------
+
+from compas_ags.ags import form_update_from_force
 
 # modify the geometry of the force diagram moving nodes further at right to the left
 move_vertices = [0, 9, 8]
-translation = +1.0
+translation = +8.0
 for key in move_vertices:
     x0 = force.vertex_attribute(key, 'x')
     force.vertex_attribute(key, 'x', x0 + translation)
@@ -106,11 +136,10 @@ form_update_from_force(form, force)
 viewer = Viewer(form, force, delay_setup=False)
 
 viewer.draw_form(lines=form_lines,
-                 forces_on=True,
+                 forces_on=False,
                  vertexlabel={key: key for key in form.vertices()},
                  external_on=False,
                  vertexsize=0.2,
-                 vertexcolor={key: '#000000' for key in fixed},
                  edgelabel={uv: index for index, uv in enumerate(form.edges())}
                  )
 
