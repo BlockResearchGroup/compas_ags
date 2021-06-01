82951f547f9d9668edf53e597892528ceb4bd6b3
Li <leochen1990@gmail.com>
2021-06-01 15:31:26 +0200

    update workflow

--- src/compas_ags/install.py
+++ src/compas_ags/install.py
@@ -75,6 +75,15 @@ if __name__ == '__main__':
 
     print("\n", "-"*10, "Installing COMPAS packages", "-"*10)
 
+    if not is_dev:
+        os.environ["CONDA_PREFIX"] = ""
+        os.environ["CONDA_DEFAULT_ENV"] = ""
+        os.environ["CONDA_EXE"] = ""
+
+    print("CONDA_PREFIX", os.environ["CONDA_PREFIX"])
+    print("CONDA_DEFAULT_ENV", os.environ["CONDA_DEFAULT_ENV"])
+    print("CONDA_EXE", os.environ["CONDA_EXE"])
+
     install(packages=PACKAGES, version=args.rhino_version)
 
     print("\n", "-"*10, "Installation is successful", "-"*10)
