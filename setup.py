9e99308ea0b38e25f23a06da5acd36d155c310bc
Licini <leochen1990@gmail.com>
2021-10-21 15:02:26 +0200

    Bump version to 1.1.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ optional_requirements = {
 
 setup(
     name='compas-ags',
-    version='1.1.1rc0',
+    version='1.1.1',
     description='COMPAS package for Computational Graphic Statics',
     long_description=long_description,
     long_description_content_type='text/markdown',
