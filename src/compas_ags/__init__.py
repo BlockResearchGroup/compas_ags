282cd4ba6420ff1621d0480e405d6e77b8f0e585
tomvanmele <tom.v.mele@gmail.com>
2024-11-27 09:57:38 +0100

    Bump version to 1.3.3

--- src/compas_ags/__init__.py
+++ src/compas_ags/__init__.py
@@ -10,7 +10,7 @@ __author__ = "Tom Van Mele and others"
 __copyright__ = "Copyright 2014-2018 - Block Research Group, ETH Zurich"
 __license__ = "MIT License"
 __email__ = "vanmelet@ethz.ch"
-__version__ = "1.3.2"
+__version__ = "1.3.3"
 
 
 def get(relpath):
